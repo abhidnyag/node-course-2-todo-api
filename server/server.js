429b9d86422a9ca77c55ffab9348e54f33e16a89
abhidnyag <abhidnyag123@gmail.com>
2017-12-16 15:05:26 +0530

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -119,9 +119,18 @@ var body = _.pick(req.body, ['email', 'password']);
  }).catch((e) => {
   res.status(400).send();
  });
-
 //res.send(body); 
 });
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+      res.status(200).send();
+    }, () => {
+      res.status(400).send();
+    });
+});
+
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
